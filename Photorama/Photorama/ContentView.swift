af1e0676b318650e372182d4f13b4fe3ad17682e
nirmohi <56351358+nirmohipatel1@users.noreply.github.com>
2022-08-05 14:03:48 -0400

    Add files via upload

---
+++ Photorama/Photorama/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Photorama
+//
+//  Created by Sahil Patel on 2020-04-26.
+//  Copyright © 2020 Nirmohi Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
